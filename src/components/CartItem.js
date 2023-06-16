c8eb8002419f033fdcaa2f3473e2b707dbf017dc
samuel922 <onyangosamuel583@gmail.com>
2023-06-15 19:18:07 -0700

    accidentally deleted my code and had to redo

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,19 +1,17 @@
 import React from "react";
 
 class CartItem extends React.Component {
-
     constructor() {
         super();
         this.state = {
+            title: "Mobile Phone",
             price: 10000,
-            title: 'Mobile Phone',
             qty: 1,
-            img: ''
+            imgUrl: ""
         }
     }
 
     increaseQuantity = () => {
-        // console.log(this.state);
         this.setState((prevState) => (
             {...prevState, qty: prevState.qty + 1}
         ))
@@ -21,53 +19,53 @@ class CartItem extends React.Component {
 
     decreaseQuantity = () => {
         this.setState((prevState) => (
-            {...prevState, qty: prevState.qty - 1}
+            {...prevState, qty: prevState.qty -1}
         ))
     }
 
     render() {
-        const { price, title, qty } = this.state;
+        const styels = {
+            image: {
+                width: 110,
+                height: 110,
+                borderRadius: 4,
+                background: '#ccc'
+            }
+        }
+
+        const { title, price, qty } = this.state;
         return (
             <div className="cart-item">
                 <div className="left-block">
-                    <img style={styles.image} />
+                    <img style={styels.image} />
                 </div>
                 <div className="right-block">
                     <h3 style={{ fontSize: 25 }}>{title}</h3>
-                    <p style={{ color: '#777' }}>Ksh.{price}</p>
-                    <p style={{ color: '#777'}}>Qty: {qty}</p>
+                    <p style={{ color: "#777" }}>Price: Ksh.{price}</p>
+                    <p style={{ color: "#777" }}>Qty: {qty}</p>
                     <div className="cart-item-actions">
-                        {/* Buttons */}
                         <img 
-                            className="action-icons" 
+                            src="https://cdn-icons-png.flaticon.com/128/992/992651.png" 
                             alt="increase" 
-                            src="https://cdn-icons-png.flaticon.com/128/3024/3024515.png" 
+                            className="action-icons" 
                             onClick={this.increaseQuantity}
+
                         />
                         <img 
-                            className="action-icons" 
-                            alt="decrease" 
                             src="https://cdn-icons-png.flaticon.com/128/1828/1828906.png" 
+                            alt="decrease" 
+                            className="action-icons" 
                             onClick={this.decreaseQuantity}
                         />
                         <img 
-                            className="action-icons" 
-                            alt="delete" 
                             src="https://cdn-icons-png.flaticon.com/128/1214/1214428.png" 
+                            alt="delete" 
+                            className="action-icons" 
                         />
                     </div>
                 </div>
             </div>
         );
-    }
-}
-
-const styles = {
-    image: {
-        height: 110,
-        width: 110,
-        borderRadius: 4,
-        background: '#ccc'
     }
 }
 
